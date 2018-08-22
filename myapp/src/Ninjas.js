8519411008973d2c278a9e421b72bdebad333485
Jim McGee <jmcgee86@gmail.com>
2018-08-22 16:48:37 -0400

    completed tutorial

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,24 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-    render(){
-        //console.log(this.props)
-        const { ninjas } = this.props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className ="ninja" key={ninja.id}>
-                    <div>Name: { ninja.name }</div>
-                    <div>Age: { ninja.age }</div>
-                    <div>Belt: { ninja.belt }</div>
-                </div>
-            )
+const Ninjas = ({ninjas, deleteNinja}) => {
+        // const ninjaList = ninjas.map(ninja => {
+        //     if(ninja.age > 20){
+        //     return (
+        //         <div className ="ninja" key={ninja.id}>
+        //             <div>Name: { ninja.name }</div>
+        //             <div>Age: { ninja.age }</div>
+        //             <div>Belt: { ninja.belt }</div>
+        //         </div>
+        //     )
+        // } else {
+        //     return null
+        // }
+        // })
+        const ninjaList = ninjas.map(ninja =>{
+          return ninja.age > 20 ? (
+            <div className ="ninja" key={ninja.id}>
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
+                <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
+            </div>
+          ) : null;
         })
         return(
             <div className = "ninja-list">
                 { ninjaList }
             </div>
         )
-    }
 }
 
 export default Ninjas
