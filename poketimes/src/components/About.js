993e6e83dc1d18e895f65722ba9a426cf07caa36
Jim McGee <jmcgee86@gmail.com>
2018-08-31 17:22:09 -0400

    complete through section 27

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div className = "container">
+            <h4 className = "center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, quae eum asperiores fugit qui excepturi repellendus magnam soluta natus odio aut veritatis rem eos porro unde vel. Iure, possimus voluptatum.</p>
+        </div>
+    )
+}
+
+export default About
